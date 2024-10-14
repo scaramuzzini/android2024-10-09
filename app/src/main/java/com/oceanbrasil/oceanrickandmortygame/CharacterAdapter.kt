5fe9af8bde7c94462c2b2d5965e9c0fae89c84a8
Mario Torres <scaramuzzini@gmail.com>
2024-10-14 09:01:09 -0400

    Character item

--- app/src/main/java/com/oceanbrasil/oceanrickandmortygame/CharacterAdapter.kt
+++ app/src/main/java/com/oceanbrasil/oceanrickandmortygame/CharacterAdapter.kt
@@ -3,8 +3,10 @@ package com.oceanbrasil.oceanrickandmortygame
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class CharacterAdapter
     (private val characters: List<Character>)
@@ -12,6 +14,8 @@ class CharacterAdapter
 {
         class CharacterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val nameTextView: TextView = view.findViewById(R.id.character_name)
+            val statusTextView: TextView = view.findViewById(R.id.character_status)
+            val imageView: ImageView = view.findViewById(R.id.character_image)
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
@@ -27,5 +31,7 @@ class CharacterAdapter
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
         val character = characters[position]
         holder.nameTextView.text = character.name
+        holder.statusTextView.text = character.status
+        Glide.with(holder.itemView).load(character.image).into(holder.imageView);
     }
 }
